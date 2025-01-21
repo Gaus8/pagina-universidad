fb5c5c20d647c4e30a99da38c460fcfa11716dee
Stiven Garzon <usg210802@outlook.com>
2025-01-20 19:45:19 -0500

    Conexion y funcionamiento de metodo ppost

--- backend/controllers/controllerRegistro.js
+++ backend/controllers/controllerRegistro.js
@@ -1,15 +1,27 @@
 import asyncHandler from 'express-async-handler';
+import { validateUser } from '../esquema/validateString.js';
+import bcrypt from 'bcrypt';
 
 export const registrarUsuario = asyncHandler(async (req, res) => {
-  const { nombres, email, password } = req.body;
-  if (!nombres || !email || !password) {
+  const { name, email, password } = req.body;
+  if (!name || !email || !password) {
     return res.status(400).json({ message: 'Todos los campos deben ser llenados' });
   }
+
+  const validar = validateUser(req.body);
+  if (validar.error) {
+    return res.status(400).json({ error: JSON.parse(validar.error.message) });
+  }
+
+  const hashedPassword = await bcrypt.hash(validar.data.password, 10);
+  const newUser = {
+    name: validar.data.name,
+    email: validar.data.email,
+    password: hashedPassword
+  };
+
   res.status(201).json({
-    nombres,
-    email,
-    password
+    newUser
   });
-  console.log(`Nombre: ${nombres} Email: ${email} Pass: ${password}`);
 });
 
--- public/scripts/script_registro.js
+++ public/scripts/script_registro.js
@@ -1,59 +1,51 @@
-
-const  botonVentanaInicio = document.getElementById('boton-ventana-inicio');
-const botonRegistroUsuarios= document.getElementById('boton-registro-usuarios');
-
+const botonVentanaInicio = document.getElementById('boton-ventana-inicio');
+const botonRegistroUsuarios = document.getElementById('boton-registro-usuarios');
+const textoError = document.querySelectorAll('.errores');
 const nombreValue = document.getElementById('nombres-registro');
 const emailValue = document.getElementById('email-registro');
 const passwordValue = document.getElementById('password-registro');
 
-const recibirDatos = async (url='',data={}) =>{
-    try{
-        const response = await fetch(url,{
-            method:'POST',
-            mode: 'cors',
-            cache:'no-cache',
-            credentials: 'same-origin',
-            headers:{
-                'Content-Type': 'application/json'
-            },
-            redirect: 'follow',
-            referrerPolicy: 'no-referrer',
-            body: JSON.stringify(data)
+const registerUser = async (url, data = {}) => {
+  try {
+    const response = await fetch(url, {
+      method: 'POST',
+      body: JSON.stringify(data),
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    });
+
+    if (!response.ok) {
+      const errorData = await response.json();
+      if (errorData.error) {
+        textoError.forEach(elemento => {
+          elemento.style.display = 'block';
         });
-        if(!response.ok){
-            throw new Error(`Error en la solicitud: ${response.status}`)
-        }
-        return await response.json(data);
-    }catch (error){
-        console.error('Error al realizar la solicitud:', error);
-        throw error;
+      }
     }
-    }
-   
+    const responseData = await response.json();
+    console.log(responseData);
+  } catch (error) {
+    console.log('ERROR: ', error.message);
+  }
+};
 
+botonRegistroUsuarios.addEventListener('click', async (event) => {
+  event.preventDefault();
+  const name = nombreValue.value;
+  const email = emailValue.value;
+  const password = passwordValue.value;
 
-botonRegistroUsuarios.addEventListener('click',async (event) =>{
-    event.preventDefault();
-    const nombres = nombreValue.value;
-    const email = emailValue.value;
-    const password = passwordValue.value;
-    
-    if(!nombres || !email || !password){
-       return window.alert("Asegurese de llenar todos lo campos");
-    }
-    try{
-       const resultado = await recibirDatos('/inicio/registro',{ nombres, email, password });
-       window.alert('Registro Exitoso');
-       console.log('Respuesta del servidor:', resultado);
-    }catch (error){
-
-    }
+  if (!name || !email || !password) {
+    return window.alert('Asegurese de llenar todos lo campos');
+  };
+  registerUser('/inicio/registro', { name, email, password });
 });
 
-botonVentanaInicio.addEventListener('click',(event) =>{
-    event.preventDefault();
-    setTimeout(()=>{
-        window.location.href = '/inicio/index.html';
-    },200);
-})
+botonVentanaInicio.addEventListener('click', (event) => {
+  event.preventDefault();
+  setTimeout(() => {
+    window.location.href = '/inicio/index.html';
+  }, 200);
+});
 
