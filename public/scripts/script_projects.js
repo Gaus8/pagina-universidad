7638140ed01b4bebca4eb1e6c9056c448c246134
Stiven <usg210802@outlook.com>
2025-02-06 19:34:35 -0500

    Insercion del logout en todos los scripts

--- public/scripts/script_projects.js
+++ public/scripts/script_projects.js
@@ -198,3 +198,19 @@ async function getDataPatch () {
   finalText.innerHTML = 'Enviando proyecto ...';
   await updateProject('/students/projects', formData);
 }
+
+
+
+document.getElementById('profile-icon').addEventListener('click', function () {
+  const menu = document.getElementById('dropdown-menu');
+  menu.style.display = menu.style.display === 'block' ? 'none' : 'block';
+});
+
+document.addEventListener('click', function (event) {
+  const profileIcon = document.getElementById('profile-icon');
+  const menu = document.getElementById('dropdown-menu');
+
+  if (!profileIcon.contains(event.target) && !menu.contains(event.target)) {
+    menu.style.display = 'none';
+  }
+});
